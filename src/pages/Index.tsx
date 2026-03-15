20abe64c6e0a64ba863295d0196176355ff05700
Nicolas Sanchez Calderon <nicolas.sanchez06@usa.edu.co>
2026-03-15 14:14:31 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,28 +1,45 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
+import CatalogSection from "@/components/CatalogSection";
+import AboutSection from "@/components/AboutSection";
+import FooterSection from "@/components/FooterSection";
 import FeaturedProduct from "@/components/FeaturedProduct";
 import BrandBenefits from "@/components/BrandBenefits";
-import CatalogSection from "@/components/CatalogSection";
 import ArtisanProcess from "@/components/ArtisanProcess";
 import VisualGallery from "@/components/VisualGallery";
 import Testimonials from "@/components/Testimonials";
 import CTASection from "@/components/CTASection";
-import AboutSection from "@/components/AboutSection";
-import FooterSection from "@/components/FooterSection";
+import ProductDetailPage from "@/components/ProductDetailPage";
+import { products } from "@/data/products";
 
 const Index = () => {
+  const params = new URLSearchParams(window.location.search);
+  const productSlug = params.get("product");
+
+  if (productSlug && productSlug in products) {
+    const product = products[productSlug as keyof typeof products];
+
+    return (
+      <div className="min-h-screen bg-background">
+        <Header />
+        <ProductDetailPage product={product} />
+        <FooterSection />
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen bg-background">
       <Header />
       <HeroSection />
       <FeaturedProduct />
       <BrandBenefits />
-      <CatalogSection />
       <ArtisanProcess />
+      <CatalogSection />
+      <AboutSection />
       <VisualGallery />
       <Testimonials />
       <CTASection />
-      <AboutSection />
       <FooterSection />
     </div>
   );
